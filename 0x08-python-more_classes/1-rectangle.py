d47aadaf428bb2ebdaebca24349de8a158ab118e
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2024-01-04 18:42:29 -0800

    correctins

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
+
+
 class Rectangle:
     """Represent a rectangle."""
 
@@ -10,33 +12,31 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        self._width = 0
-        self._height = 0
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """Get/set the width of the rectangle."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        self._validate_and_set_dimension('_width', value)
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
         """Get/set the height of the rectangle."""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        self._validate_and_set_dimension('_height', value)
-
-    def _validate_and_set_dimension(self, attribute, value):
-        """Validate and set the width or height attribute."""
         if not isinstance(value, int):
-            raise TypeError(f"{attribute[1:]} must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError(f"{attribute[1:]} must be >= 0")
-        setattr(self, attribute, value)
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
+
+
 class Rectangle:
     """Represent a rectangle."""
 
@@ -20,7 +22,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        self._validate_dimension('_width', value)
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -29,31 +35,39 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        self._validate_dimension('_height', value)
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """Return the area of the Rectangle."""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
-        return 2 * (self.__width + self.__height) if self.__width != 0 and self.__height != 0 else 0
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
         Represents the rectangle with the # character.
         """
-        return '\n'.join(['#' * self.__width for _ in range(self.__height)])
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        return f"Rectangle({self.__width}, {self.__height})"
-
-    def _validate_dimension(self, attribute, value):
-        """Validate and set the width or height attribute."""
-        if not isinstance(value, int):
-            raise TypeError(f"{attribute[1:]} must be an integer")
-        if value < 0:
-            raise ValueError(f"{attribute[1:]} must be >= 0")
-        setattr(self, attribute, value)
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
